2433e79ac6c49ea5f742319542923c9584b4a3a1
Christoph Gockel <github@christophgockel.de>
2016-04-23 22:19:11 +0100

    Initial commit

---
+++ SomeApp/SomeAppTests/SomeAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  SomeAppTests.swift
+//  SomeAppTests
+//
+//  Created by Christoph Gockel on 23/04/2016.
+//  Copyright © 2016 8th Light. All rights reserved.
+//
+
+import XCTest
+@testable import SomeApp
+
+class SomeAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
